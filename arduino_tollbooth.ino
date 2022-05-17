b79567ec6708af2050f879c3c228324a5a938f1d
Kenneth Lin <70595901+Logypsyc@users.noreply.github.com>
2022-05-17 14:31:27 -0400

    Add files via upload

---
+++ arduino_tollbooth.ino
@@ -0,0 +1,15 @@
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(12, INPUT); //pushbutton up
+  pinMode(11, INPUT); //pushbutton down
+  pinMode(10, INPUT); //pushbutton reset
+  pinMode(9, INPUT); //upper limit
+  pinMode(8, INPUT); //lower limit
+  pinMode(7, OUTPUT); //motor
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  
+
+}
