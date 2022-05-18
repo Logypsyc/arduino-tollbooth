a4c3ead37d4a3cb8bf5eaa2b0fdbbd32f2cd86a4
Kenneth Lin <70595901+Logypsyc@users.noreply.github.com>
2022-05-18 10:22:12 -0400

    Add files via upload

--- arduino_tollbooth.ino
+++ arduino_tollbooth.ino
@@ -8,8 +8,36 @@ void setup() {
   pinMode(7, OUTPUT); //motor
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-  
+int buttonPressed(uint8_t button) {
+  static uint16_t lastStates = 0;
+  uint8_t state = digitalRead(button);
+  if (state != ((lastStates >> button) & 1)) {
+    lastStates ^= 1 << button;
+    return state == HIGH;
+  }
+  return false;
+} 
 
+void loop() {
+  int upperLimitState = digitalRead(9);
+  int lowerLimitState = digitalRead(8);
+  // put your main code here, to run repeatedly:
+  if (buttonPressed(12)) {
+    while (true) {
+      digitalWrite(7, HIGH);  
+      if (upperLimitState) {
+        break;
+      }
+    }
+  }
+
+  if (buttonPressed(11)) {
+    while (true) {
+      digitalWrite(7, ! HIGH);  
+      if (lowerLimitState) {
+        break;
+      }
+    }
+  }
+  
 }
