120ed1dbb8807a7d1bc888a0f8ffc21cf6b51e9b
Kenneth Lin <70595901+Logypsyc@users.noreply.github.com>
2022-05-18 14:31:53 -0400

    Add files via upload

--- arduino_tollbooth.ino
+++ arduino_tollbooth.ino
@@ -1,39 +1,34 @@
 void setup() {
   // put your setup code here, to run once:
-  pinMode(12, INPUT); //pushbutton up
-  pinMode(11, INPUT); //pushbutton down
-  pinMode(10, INPUT); //pushbutton reset
+  pinMode(13, OUTPUT);
+  Serial.begin(9600);
+  pinMode(12, INPUT_PULLUP); //pushbutton up
+  pinMode(11, INPUT_PULLUP); //pushbutton down
+  pinMode(10, INPUT_PULLUP); //pushbutton reset
   pinMode(9, INPUT); //upper limit
   pinMode(8, INPUT); //lower limit
   pinMode(7, OUTPUT); //motor
 }
 
-int buttonPressed(uint8_t button) {
-  static uint16_t lastStates = 0;
-  uint8_t state = digitalRead(button);
-  if (state != ((lastStates >> button) & 1)) {
-    lastStates ^= 1 << button;
-    return state == HIGH;
-  }
-  return false;
-} 
 
 void loop() {
   int upperLimitState = digitalRead(9);
   int lowerLimitState = digitalRead(8);
   // put your main code here, to run repeatedly:
-  if (buttonPressed(12)) {
+  if (digitalRead(12) == HIGH) {
     while (true) {
-      digitalWrite(7, HIGH);  
+      digitalWrite(7, HIGH); 
+      digitalWrite(13, HIGH);  
       if (upperLimitState) {
         break;
       }
     }
   }
 
-  if (buttonPressed(11)) {
+  if (digitalRead(11) == HIGH) {
     while (true) {
-      digitalWrite(7, ! HIGH);  
+      digitalWrite(7, ! HIGH);
+      digitalWrite(13, LOW);   
       if (lowerLimitState) {
         break;
       }
