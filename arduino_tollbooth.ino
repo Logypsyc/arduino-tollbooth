536595d29088653c741227dcd9ad15a3507a78b0
Kenneth Lin <70595901+Logypsyc@users.noreply.github.com>
2022-05-20 15:26:14 -0400

    Add files via upload

--- arduino_tollbooth.ino
+++ arduino_tollbooth.ino
@@ -1,38 +1,57 @@
 void setup() {
   // put your setup code here, to run once:
   pinMode(13, OUTPUT);
-  Serial.begin(9600);
-  pinMode(12, INPUT_PULLUP); //pushbutton up
-  pinMode(11, INPUT_PULLUP); //pushbutton down
-  pinMode(10, INPUT_PULLUP); //pushbutton reset
-  pinMode(9, INPUT); //upper limit
-  pinMode(8, INPUT); //lower limit
-  pinMode(7, OUTPUT); //motor
+  pinMode(12, INPUT_PULLUP); //button up
+  pinMode(11, INPUT_PULLUP); //button down
+  pinMode(10, INPUT_PULLUP); //upper limit
+  pinMode(9, INPUT_PULLUP); //lower limit
+  pinMode(8, OUTPUT); //motor
 }
 
 
 void loop() {
-  int upperLimitState = digitalRead(9);
-  int lowerLimitState = digitalRead(8);
-  // put your main code here, to run repeatedly:
-  if (digitalRead(12) == HIGH) {
-    while (true) {
-      digitalWrite(7, HIGH); 
-      digitalWrite(13, HIGH);  
-      if (upperLimitState) {
-        break;
-      }
+  if (buttonPressed(12)) {
+    //flash led
+    digitalWrite(13, HIGH);
+    delay(100);
+    digitalWrite(13, LOW);
+
+    //turn motor on
+    digitalWrite(8, HIGH);
+   
+    if (buttonPressed(10)) {
+      //turn motor off
+      digitalWrite(8, LOW);
+      //turn led on
+      digitalWrite(13, HIGH);
     }
   }
 
-  if (digitalRead(11) == HIGH) {
-    while (true) {
-      digitalWrite(7, ! HIGH);
-      digitalWrite(13, LOW);   
-      if (lowerLimitState) {
-        break;
-      }
+  if (buttonPressed(11)) {
+    //flash led
+    digitalWrite(13, HIGH);
+    delay(100);
+    digitalWrite(13, LOW);
+    
+    //turn motor on in other direction
+    digitalWrite(8, ! HIGH);
+
+    
+    if (buttonPressed(9)) {
+      //turn motor off
+      digitalWrite(9, LOW);
+      digitalWrite(13, LOW);
     }
   }
-  
+
+}
+
+int buttonPressed(uint8_t button) {
+  static uint16_t lastStates = 0;
+  uint8_t state = digitalRead(button);
+  if (state != ((lastStates >> button) & 1)) {
+    lastStates ^= 1 << button;
+    return state == HIGH;
+  }
+  return false;
 }
