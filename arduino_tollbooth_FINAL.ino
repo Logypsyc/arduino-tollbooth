32d4f7938b1344ee0f39ccc4459722b99ea68792
Kenneth Lin <70595901+Logypsyc@users.noreply.github.com>
2022-05-25 14:23:50 -0400

    Update arduino_tollbooth_FINAL.ino

--- arduino_tollbooth_FINAL.ino
+++ arduino_tollbooth_FINAL.ino
@@ -12,33 +12,23 @@ void setup() {
 
 
 void loop() {
-  if (buttonPressed(12)) {
+  if (digitalRead(12) == HIGH) {
+    do {
     digitalWrite(6, LOW); // set leg 1 of the H-bridge low
     digitalWrite(7, HIGH); // set leg 2 of the H-bridge high
-    analogWrite(3, 127);
-    if (buttonPressed(10)) {
+    analogWrite(3, 255);
+    } while (digitalRead(10) == HIGH);
       digitalWrite(6, LOW); // set leg 1 of the H-bridge low
       digitalWrite(7, LOW);
-    }
   }
 
-  if (buttonPressed(11)) {
+  if (digitalRead(11) == HIGH) {
+    do {
     digitalWrite(6, HIGH); // set leg 1 of the H-bridge low
     digitalWrite(7, LOW); // set leg 2 of the H-bridge high
-    analogWrite(3, -127);
-    if (buttonPressed(9)) {
-      digitalWrite(6, LOW); // set leg 1 of the H-bridge low
-      digitalWrite(7, LOW);
-    }
+    analogWrite(3, 255);
+    } while (digitalRead(9) == HIGH);
+    digitalWrite(6, LOW); // set leg 1 of the H-bridge low
+    digitalWrite(7, LOW);
   }
-}
-
-int buttonPressed(uint8_t button) {
-  static uint16_t lastStates = 0;
-  uint8_t state = digitalRead(button);
-  if (state != ((lastStates >> button) & 1)) {
-    lastStates ^= 1 << button;
-    return state == HIGH;
-  }
-  return false;
 }
